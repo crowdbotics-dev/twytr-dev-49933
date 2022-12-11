aa7fd10c457348063f2dd5c125cff869158d46d4
Crowdbotics <engineering@crowdbotics.com>
2022-12-11 15:28:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/twytr_dev_49933/MainActivity.java
@@ -0,0 +1,15 @@
+package com.twytr_dev_49933;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "twytr_dev_49933";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twytr-dev-49933",
+  url: "https://twytr-dev-49933.botics.co",
+  api: "https://twytr-dev-49933.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
